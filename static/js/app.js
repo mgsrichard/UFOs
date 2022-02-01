33876accc02bf53912b526f54ef1599ec66e7015
Martha Richardson <mgsrichard@yahoo.com>
2022-02-01 10:13:44 -0500

    end of 11.5.4

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,8 @@ const tableData = data;
 //Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+
+//begin definition of buildTable function
 function buildTable(data) {
 
     //Clear any existing data
@@ -24,4 +26,35 @@ function buildTable(data) {
         );
 
     });
-}
+};
+//end defintion of buildTable function
+
+//begin definition of handleClick function
+function handleClick() {
+
+    //Grab the datetime value from the filter
+    let date = d3.select("datetime").property("value");
+    let filteredData = tableData;
+
+    //Check to see if a date was entered and filter the
+    //date using that date
+    if(date) {
+        //apply filter to the table data to only keep the rows
+        //where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //Rebuild the table using filtered data
+    // NOTE: if no date was entered, then filteredData will
+    //just be the original tableData
+    buildTable(filteredData);
+};
+//end definition of handleClick function
+
+//Listen for a click, execute the handleCllick function when the button in the 
+//tag filter-btn is clicked
+d3.selectAll("#filter-btn").on("click",handleClick);
+
+
+//Build the table when the page loads
+buildTable(tableData);
